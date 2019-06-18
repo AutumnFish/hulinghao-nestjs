82f6d526eecad77cc4c86127e9a7312a34d3f5e3
autumnfish <517729329@qq.com>
2019-06-18 13:39:19 +0800

    实现useing-servers

--- src/posts/providers/demo/demo.service.ts
+++ src/posts/providers/demo/demo.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from 'src/posts/interfaces/post.interface';
 
 @Injectable()
-export class DemoService {}
+export class DemoService {
+  // 私有的Posts
+  private readonly posts: Post[] = [];
+  // 获取所有
+  findAll(): Post[] {
+    return this.posts;
+  }
+  // 往posts添加数据的方法
+  create(post: Post) {
+    this.posts.push(post);
+  }
+}
